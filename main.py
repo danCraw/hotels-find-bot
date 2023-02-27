c291d777303bbf9b46cd1c3dccadf77af8f847b6
Misticksman <vadim02101@gmail.com>
2023-02-28 02:59:54 +0300

    test geopy.Yandex

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from aiogram import types, utils, executor
 from create_bot import dp
 
 from geopy.geocoders import Yandex
-from config import TOKEN, YANDEX_API
+from config import YANDEX_API
 
 geolocator = Yandex(YANDEX_API)
+location = geolocator.geocode('Voronezh')
 
 async def on_startup(_):
     print('The bot has started')
@@ -17,4 +18,5 @@ client.client_handler_register(dp)
 other.other_handler_register(dp)  # ниже всех других handler's
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+    # executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+    print(location.latitude, location.longitude)
