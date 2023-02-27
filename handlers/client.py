f77ccc60d4531df3a689dab47738efe44544ba1b
Misticksman <vadim02101@gmail.com>
2023-02-27 22:32:27 +0300

    add print location in chat

--- handlers/client.py
+++ handlers/client.py
@@ -6,17 +6,30 @@ from keyboards import kb_client
 async def start_command(message: types.Message):
     await bot.send_message(message.from_user.id, 'Hello', reply_markup=kb_client)
 
+''' INFO'''
 # @dp.message_handler(commands=['info'])
 async def get_info(message: types.Message):
     await message.answer("Some info")
 
 
-# @dp.message_handler(commands=['location'])
-async def get_location(message: types.Message):
-    await bot.send_message(message.from_user.id, 'location')
+'''MY LOCATION'''
+
+# def get_keyboard():
+#     keyboard = types.ReplyKeyboardMarkup()
+#     button = types.KeyboardButton("Share Position", request_location=True)
+#     keyboard.add(button)
+#     return keyboard
+# @dp.message_handler(content_types=['location'])
+
+# @dp.message_handler(content_types=['location'])
+async def my_location(message: types.Message):
+    lat = message.location.latitude
+    lon = message.location.longitude
+    coordinates = f"latitude:{lat}\nlongitude:{lon}"
+    await message.answer(coordinates)
 
 
 def client_handler_register(dp: Dispatcher):
     dp.register_message_handler(start_command, commands=['start', 'help'])
     dp.register_message_handler(get_info, commands=['info'])
-    dp.register_message_handler(get_location, commands='location')
+    dp.register_message_handler(my_location, content_types=['location'])
--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -1,10 +1,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 b1 = KeyboardButton('/info')
-b2 = KeyboardButton('/location')
+# b2 = KeyboardButton('/locate')
 
 b_phone = KeyboardButton('Phone', request_contact=True)
-b_location = KeyboardButton('Location', request_location=True)
+b_location = KeyboardButton('My location', request_location=True)
+
+
 
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
-kb_client.add(b1).add(b2).row(b_phone, b_location)
+kb_client.add(b1).row(b_phone, b_location)
